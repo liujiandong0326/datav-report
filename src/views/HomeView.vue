6142a26fb91821651182938168295127d6d32347
liujd2 <liujd2@lenovo.com>
2022-08-23 13:59:40 +0800

    feat: 添加 element 和 echarts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,34 @@
 <template>
-  <div class="home">aaa</div>
+  <div class="home">
+    <top-view />
+    <sales-view />
+    <map-view />
+    <bottom-view />
+  </div>
 </template>
 
 <script>
-export default {};
+import TopView from "@/components/TopView";
+import SalesView from "@/components/SalesView";
+import MapView from "@/components/MapView";
+import BottomView from "@/components/BottomView";
+
+export default {
+  components: {
+    TopView,
+    SalesView,
+    MapView,
+    BottomView,
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
