c06a85ff9e93ca40ed3add73c38ee41bf32127d7
mike <liujiandong0326@163.com>
2022-08-23 21:48:41 +0800

    feat: 通用组件封装

--- src/components/TopView/index.vue
+++ src/components/TopView/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>top view</div>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card shadow="hover">
+          <total-sales />
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          <total-orders />
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          <today-users />
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card shadow="hover">
+          <total-users />
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-export default {};
+import TotalSales from '../TotalSales'
+import TotalUsers from '../TotalUsers'
+import TodayUsers from '../TodayUsers'
+import TotalOrders from '../TotalOrders'
+
+export default {
+  components: {
+    TotalSales,
+    TotalUsers,
+    TodayUsers,
+    TotalOrders,
+  },
+}
 </script>
 
 <style scoped></style>
---
+++ src/components/TotalSales/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <common-card title="累计销售额" value="$ 32,039,165">
+    <template v-slot:footer>
+      <span>昨日销售额</span>
+      <span class="money">$ 30,000,000</span>
+    </template>
+  </common-card>
+</template>
+
+<script>
+import commonCardMixin from '@/mixins/commonCardMixin'
+
+export default {
+  mixins: [commonCardMixin],
+}
+</script>
+
+<style scoped lang="scss">
+.money {
+  margin-left: 5px;
+  color: #333;
+  font-weight: 700;
+}
+</style>
