0bcc820672dec27592658a60cfb6fcedb210074f
liujd2 <liujd2@lenovo.com>
2022-08-24 13:31:47 +0800

    feat: 完成今日交易用户数

--- src/components/TodayUsers/index.vue
+++ src/components/TodayUsers/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <common-card />
+  <common-card title="今日交易用户数" value="81,014">
+    <template>
+      <div id="today-users-chart" :style="{ width: '100%', height: '100%' }" />
+    </template>
+    <template v-slot:footer>
+      <span>退货率</span>
+      <span class="emphasize">5.14%</span>
+    </template>
+  </common-card>
 </template>
 
 <script>
@@ -7,7 +15,52 @@ import commonCardMixin from '@/mixins/commonCardMixin'
 
 export default {
   mixins: [commonCardMixin],
+  mounted() {
+    const chartDom = document.getElementById('today-users-chart')
+    const chart = this.$echarts.init(chartDom)
+
+    chart.setOption({
+      color: ['#3398DB'], // 柱状图的颜色
+      xAxis: {
+        type: 'category',
+        data: [
+          '00:00',
+          '01:00',
+          '02:00',
+          '03:00',
+          '04:00',
+          '05:00',
+          '06:00',
+          '07:00',
+          '08:00',
+          '09:00',
+          '10:00',
+          '11:00',
+          '12:00',
+          '13:00',
+        ],
+        show: false,
+        // boundaryGap: false,
+      },
+      yAxis: {
+        show: false,
+      },
+      series: [
+        {
+          type: 'bar',
+          data: [
+            410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 134, 290, 330, 150,
+          ],
+          barWidth: '60%', // 柱状图的宽度
+        },
+      ],
+      grid: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      },
+    })
+  },
 }
 </script>
-
-<style scoped></style>
--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <common-card />
+  <common-card title="累计用户数" value="1,087,503">
+    <template>
+      <div id="total-users-chart" :style="{ width: '100%', height: '100%' }" />
+    </template>
+    <template v-slot:footer>
+      <div class="total-users-footer">
+        <span>日同比</span>
+        <span class="emphasize">8.73%</span>
+        <div class="increase" />
+        <span class="month">月同比</span>
+        <span class="emphasize">35.91%</span>
+        <div class="decrease" />
+      </div>
+    </template>
+  </common-card>
 </template>
 
 <script>
@@ -7,7 +21,27 @@ import commonCardMixin from '@/mixins/commonCardMixin'
 
 export default {
   mixins: [commonCardMixin],
+  mounted() {
+    const chartDom = document.getElementById('total-users-chart')
+    const chart = this.$echarts.init(chartDom)
+
+    chart.setOption({
+      grid: {},
+      xAxis: {},
+      yAxis: {},
+      series: [],
+    })
+  },
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.total-users-footer {
+  display: flex;
+  align-items: center;
+
+  .month {
+    margin-left: 10px;
+  }
+}
+</style>
