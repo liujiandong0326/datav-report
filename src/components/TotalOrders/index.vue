d6f4a388e9358fd97e082f00c14625dd18b3c839
liujd2 <liujd2@lenovo.com>
2022-08-25 13:35:44 +0800

    feat: 完成头部布局

--- src/components/TotalOrders/index.vue
+++ src/components/TotalOrders/index.vue
@@ -1,7 +1,7 @@
 <template>
   <common-card title="累计订单量" value="2,157,420">
     <template>
-      <div id="total-orders-chart" :style="{ width: '100%', height: '100%' }" />
+      <v-chart :option="getOption()" />
     </template>
     <template v-slot:footer>
       <span>昨日订单量</span>
@@ -15,47 +15,46 @@ import commonCardMixin from '@/mixins/commonCardMixin'
 
 export default {
   mixins: [commonCardMixin],
-  mounted() {
-    const chartDom = document.getElementById('total-orders-chart')
-    const chart = this.$echarts.init(chartDom)
-
-    chart.setOption({
-      xAxis: {
-        type: 'category',
-        show: false, // 隐藏坐标系
-        boundaryGap: false, // true：默认的时候会在左右两边有间距
-      },
-      yAxis: {
-        show: false, // 隐藏坐标系
-      },
-      series: [
-        {
-          type: 'line',
-          data: [
-            620, 432, 220, 534, 790, 430, 220, 320, 532, 320, 834, 690, 530,
-            220, 620,
-          ],
-          areaStyle: {
-            color: 'purple',
-          },
-          // 线条
-          lineStyle: {
-            width: 0,
-          },
-          // 线条上的点
-          itemStyle: {
-            opacity: 0,
-          },
-          smooth: true,
+  methods: {
+    getOption() {
+      return {
+        xAxis: {
+          type: 'category',
+          show: false, // 隐藏坐标系
+          boundaryGap: false, // true：默认的时候会在左右两边有间距
         },
-      ],
-      grid: {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-      },
-    })
+        yAxis: {
+          show: false, // 隐藏坐标系
+        },
+        series: [
+          {
+            type: 'line',
+            data: [
+              620, 432, 220, 534, 790, 430, 220, 320, 532, 320, 834, 690, 530,
+              220, 620,
+            ],
+            areaStyle: {
+              color: 'purple',
+            },
+            // 线条
+            lineStyle: {
+              width: 0,
+            },
+            // 线条上的点
+            itemStyle: {
+              opacity: 0,
+            },
+            smooth: true,
+          },
+        ],
+        grid: {
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        },
+      }
+    },
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./plugins/element.js";
-import * as Echarts from "echarts";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import * as Echarts from 'echarts'
+import VueECharts from 'vue-echarts'
+import './assets/style/index.css'
 
-Vue.config.productionTip = false;
-Vue.prototype.$echarts = Echarts;
+Vue.config.productionTip = false
+Vue.prototype.$echarts = Echarts
+Vue.component('v-chart', VueECharts)
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
